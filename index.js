650709a070c5cadd9f8e8e736fd802ec606764a5
Irina Selina <iras198308@gmail.com>
2021-03-13 23:48:49 +0200

    Добавляет карусель

--- index.js
+++ index.js
@@ -1,58 +1,62 @@
-import cart from "./gallery-items.js"
-
-const makeList = document.querySelector('.gallery');
-const modalWind = document.querySelector('div.lightbox');
-const lightboxImage = document.querySelector('.lightbox__image');
-const buttonClose = document.querySelector('.lightbox__button')
-const lightboxOverlay = document.querySelector('.lightbox__overlay');
-
-const makeImageList = cart.reduce((acc, { preview, original, description }) => {
-    return acc + `<li class="gallery__item">
-  <a
-    class="gallery__link"
-    src=${original}
-  >
-    <img
-      class="gallery__image"
-      src=${preview}
-      data-source=${original}
-      alt=${description}
-    />
-  </a>
-</li>`
-  
-}, '')
-makeList.insertAdjacentHTML('afterbegin', makeImageList)
-
-makeList.addEventListener('click', ({ target }) => {
-  if (target.nodeName != 'IMG') {
-    return
+import gallery from './gallery-items.js';
+const galleryListRef = document.querySelector('.js-gallery');
+const modalRef = document.querySelector('.js-lightbox');
+const modalImgRef = document.querySelector('.lightbox__image');
+const closeBtnRef = document.querySelector('.lightbox__button');
+const overleyRef = document.querySelector('.lightbox__overlay');
+let activeIndex = 0;
+const markup = gallery.map(({ preview, original, description }) => {
+  return `
+    <li class="gallery__item">
+        <a
+            class="gallery__link"
+            href="${original}"
+            >
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </li>`;
+});
+galleryListRef.insertAdjacentHTML('beforeend', markup.join(''));
+galleryListRef.addEventListener('click', openModal);
+window.addEventListener('keydown', onPressArrow);
+function onPressArrow(e) {
+  if (e.key === 'ArrowLeft' && activeIndex > 0) {
+    activeIndex -= 1;
+    modalImgRef.src = gallery[activeIndex].original;
   }
-  modalWind.classList.toggle('is-open');
-  lightboxImage.src = target.dataset.source;
-   
-})
-
-buttonClose.addEventListener('click', (evn) => {
-  if (evn.target.nodeName === "BUTTON") {
-     modalWind.classList.remove('is-open');
-  lightboxImage.src = ""
+  if (e.key === 'ArrowRight' && activeIndex < gallery.length - 1) {
+    activeIndex += 1;
+    modalImgRef.src = gallery[activeIndex].original;
   }
- 
-})
-lightboxOverlay.addEventListener('click', (evn) => {
-  if (evn.target === evn.currentTarget) {
-    modalWind.classList.remove('is-open');
-  lightboxImage.src = ""
+}
+function openModal(e) {
+  e.preventDefault();
+  if (e.target.localName === 'img') {
+    modalRef.classList.add('is-open');
+    modalImgRef.src = e.target.dataset.source;
   }
-})
-window.addEventListener('keyup', (e) => {
-  
+  for (let el of markup) {
+    if (el.includes(e.target.src)) {
+      activeIndex = markup.indexOf(el);
+    }
+  }
+}
+closeBtnRef.onclick = closeModal;
+function closeModal() {
+  modalRef.classList.remove('is-open');
+  modalImgRef.removeAttribute('src');
+}
+window.addEventListener('keydown', e => {
   if (e.key === 'Escape') {
-    modalWind.classList.remove('is-open');
-  lightboxImage.src = ""
+    closeModal();
   }
-})
+});
+overleyRef.addEventListener('click', closeModal);
 
 
 
